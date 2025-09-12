47dba078e870a4921857c93e1d48c8ae2e05760d
Rasyid Nabil P <rasyidnabil06@gmail.com>
2025-09-12 16:47:19 +0700

    feat: welcome banner. first app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import feather from "feather-icons";
 import "../assets/style/style.css";
+import "../assets/style/responsives.css";
 
 export default function Header() {
    useEffect(() => {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,14 +4,35 @@ export default function Home() {
    return (
       <>
          <Header />
-         <main className="p-4">
-            <h1>Landing Page</h1>
-            <p>
-               Selamat datang di Aplikasi Vidys Trainee, dimana tempat kamu bisa
-               mengakses banyak konten olahraga untuk para penyandang
-               disabilitas dan perempuan
-            </p>
-         </main>
+         {/* Section Welcome */}
+         <section className="hero">
+            <div className="hero-content container">
+               <h1>Vidys AI Trainer</h1>
+               <p>
+                  Selamat datang di Aplikasi Vidys Trainer, dimana tempat Anda
+                  dapat mengakses banyak konten olahraga untuk para penyandang
+                  disabilitas dan perempuan untuk gaya hidup sehat.
+               </p>
+            </div>
+         </section>
+
+         {/* Section Features */}
+         <section className="features container">
+            <div className="feature">
+               <h2>AI Trainer</h2>
+               <p>
+                  Teknologi AI Canggih membantu Anda untuk melakukan gerakan
+                  olahraga sesuai kebutuhan dan kondisi saat ini.
+               </p>
+            </div>
+            <div className="feature">
+               <h2>AI Consultant</h2>
+               <p>
+                  Konsultasi dengan AI Chatbot Pintar untuk masalah kesehatan
+                  Anda.
+               </p>
+            </div>
+         </section>
       </>
    );
 }
